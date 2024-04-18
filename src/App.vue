ce3b0e4dd0b53e6e52742db0f82f151067019bfe
tll <tulanlan@jx.chinamobile.com>
2024-04-18 16:33:23 +0800

    xiugai

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <button
+      @mousedown="handleMouseDown"
+      @mouseup="handleMouseUp"
+      @click="handleClick"
+    >
+      点击我
+    </button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  methods: {
+    handleMouseDown() {
+      console.log('鼠标按下，执行事件1');
+      // 在这里执行事件1的操作
+    },
+    handleMouseUp() {
+      console.log('鼠标松开，执行事件2');
+      // 在这里执行事件2的操作
+    },
+    handleClick() {
+      console.log('按钮被点击');
+      // 如果您需要处理点击事件，也可以在这里执行相应的操作
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
